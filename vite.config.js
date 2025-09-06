dd87903dd53cbde38523450b08160c807cd20152
lion-dev <nunovc+liger@gmail.com>
2025-09-06 19:36:21 +0200

    add domain changes

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import eslint from 'vite-plugin-eslint';
 import stylelint from 'vite-plugin-stylelint';
@@ -10,7 +9,7 @@ export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
-    base: process.env.VITE_BASE_PUBLIC_PATH,
+    base: '/',
     plugins: [
       eslint({ cache: false }),
       stylelint(),
