d247c90a8e310ab3ed9291aaa8ff6d6bf1bef4ef
lion-dev <nunovc+liger@gmail.com>
2025-09-10 19:47:46 +0200

    url changes 3

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import '@/assets/styles/fonts.css';
 import '@/assets/styles/main.css';
 import '@/assets/styles/tailwind.css';
@@ -7,7 +7,7 @@ import App from '@/app.vue';
 import { routes } from '@/routes.js';
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
